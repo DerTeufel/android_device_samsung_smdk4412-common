95f838dd474dcad08ecec30aa98a6521b7f0449d
DerTeufel <dominik-kassel@gmx.de>
2013-03-23 09:00:03 +0100

    Revert "use linaro gcc 4.7.3 for kernel building"
    This reverts commit 36e5afa22b09151d11da550701e1ca5b252ac402.

--- common.mk
+++ common.mk
@@ -20,8 +20,6 @@ DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7.3
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
